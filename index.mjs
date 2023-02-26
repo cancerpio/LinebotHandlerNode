501bc940f7a1ea473c159af3b11c9764ce789a36
Cancerpio <breezesky627@gmail.com>
2023-02-26 23:11:08 +0800

    [master]  - initial project test

--- index.mjs
+++ index.mjs
@@ -1,7 +1,7 @@
 // eslint-disable-next-line import/prefer-default-export
 export const handler = async (event) => {
   // TODO implement
-  console.log(`Get event: \n${event}`);
+  console.log(`Get events: \n${event}`);
   const response = {
     statusCode: 200,
     body: JSON.stringify('Hello from Lambda!'),
